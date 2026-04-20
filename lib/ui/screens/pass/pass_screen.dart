bf949ec9b6a2e351788201f68d1821e28f00edf7
UndertaleXFrisk <eng.vireaksathya@gmail.com>
2026-04-20 20:41:21 +0700

    fix merge

--- lib/ui/screens/pass/pass_screen.dart
+++ lib/ui/screens/pass/pass_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:jis_kong/ui/screens/pass/view_model/pass_view_model.dart';
 import 'package:jis_kong/ui/screens/pass/widget/pass_selection_list.dart';
 import 'package:jis_kong/ui/screens/pass/widget/pass_status_header.dart';
@@ -48,62 +47,6 @@ class PassScreen extends StatelessWidget {
                 ),
               ),
       ),
-=======
-import 'view_model/pass_view_model.dart';
-import 'widget/pass_content.dart';
-
-class PassScreen extends StatefulWidget {
-  const PassScreen({super.key});
-
-  @override
-  State<PassScreen> createState() => _PassScreenState();
-}
-
-class _PassScreenState extends State<PassScreen> {
-  late PassViewModel _viewModel;
-
-  @override
-  void initState() {
-    super.initState();
-    _viewModel = PassViewModel();
-
-    _viewModel.addListener(_update);
-  }
-
-  void _update() => setState(() {});
-
-  @override
-  void dispose() {
-    _viewModel.removeListener(_update);
-    _viewModel.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF8F4EB),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        selectedItemColor: Colors.deepOrange,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on_outlined),
-            label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.pedal_bike_outlined),
-            label: 'My Rides',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.confirmation_num_outlined),
-            label: 'Subscription',
-          ),
-        ],
-      ),
-      body: SafeArea(child: PassContent(viewModel: _viewModel)),
->>>>>>> d56e23e35c6cb0aa15a4bdb81852732fea1b684c
     );
   }
 }
